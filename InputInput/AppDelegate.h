fd2fd2f24492c2d7d924ea748a9a9fcca688fe7c
Luavis <luaviskang@gmail.com>
2015-10-11 10:10:45 +0900

    Initial Commit

---
+++ InputInput/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InputInput
+//
+//  Created by Luavis Kang on 10/11/15.
+//  Copyright © 2015 Luavis Kang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ InputInput/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  InputInput
+//
+//  Created by Luavis Kang on 10/11/15.
+//  Copyright © 2015 Luavis Kang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
